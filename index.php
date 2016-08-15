7101a969a185595ba415bae17181c3855438d089
yamamoto-ryugo <yamamoto.ryugo@gmail.com>
2016-08-15 23:06:23 +0900

    0004[update]機能修正
    ログイン機能構築につき，セッションの管理および，ログイン画面に飛ばす機能を追加しました。

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
   require_once('setting/config.php');
   require_once('setting/functions.php');
 
+  session_start();
+
+  if (empty($_SESSION['me'])) {
+    header('Location: page/login/');
+    exit;
+  }
+
 ?>
 <!DOCTYPE html>
 <html lang="ja">
