d2bbd4de3667a673ecb5ff020d6a4b878f4b2439
yamamoto-ryugo <yamamoto.ryugo@gmail.com>
2016-08-15 22:30:59 +0900

    コミット

--- setting/config.php
+++ setting/config.php
@@ -6,3 +6,7 @@ $user = "root";
 $password = "password";
 
 $dbh = new PDO('mysql:host=' . $host . ';dbname=' . $dbname . ';charset=utf8', $user, $password);
+
+define('PASSWORD_KEY', '2mOc_t~a'); // 暗号化キー
+
+error_reporting(E_ALL & ~E_NOTICE);
