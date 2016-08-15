ac8aae56a58dc4c0973009f9003f390fa8307ebe
yamamoto-ryugo <yamamoto.ryugo@gmail.com>
2016-08-15 23:02:05 +0900

    0003[add]新規ファイル追加
    index.phpを新規追加しました。

---
+++ index.php
@@ -0,0 +1,14 @@
+<?php
+
+  require_once('setting/config.php');
+  require_once('setting/functions.php');
+
+?>
+<!DOCTYPE html>
+<html lang="ja">
+  <head>
+    <meta charset="utf-8">
+  </head>
+  <body>
+  </body>
+</html>
